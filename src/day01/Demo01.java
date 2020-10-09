66c7540fc10ac98955df8152f0fd30bb563056d3
RexJoush <jouah970918@sina.com>
2020-10-09 21:09:18 +0800

    2020.10.09

--- src/day01/Demo01.java
+++
@@ -1,4 +0,0 @@
-package day01;
-
-public class Demo01 {
-}
---
+++ src/simple/day02/ListNode.java
@@ -0,0 +1,14 @@
+package simple.day02;
+
+import java.util.Random;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(){}
+
+    ListNode(int x) {
+        val = x;
+    }
+
+}
