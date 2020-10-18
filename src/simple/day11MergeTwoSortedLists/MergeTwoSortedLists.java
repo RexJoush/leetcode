f08f179db9942a17a01e97305f0abc6796e486fd
RexJoush <joush970918@sina.com>
2020-10-18 19:19:54 +0800

    2020.10.18

--- src/simple/day11MergeTwoSortedLists/MergeTwoSortedLists.java
+++ src/simple/day11MergeTwoSortedLists/MergeTwoSortedLists.java
@@ -18,6 +18,24 @@ package simple.day11MergeTwoSortedLists;
 
  */
 public class MergeTwoSortedLists {
+
+
+    // 递归写法
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2){
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        if (l1.val < l2.val){
+            l1.next = mergeTwoLists2(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists2(l1, l2.next);
+            return l2;
+        }
+
+    }
+
+    // 常规写法
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
         // l1 为空，返回l2
