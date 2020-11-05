2485b8306ed8d0dc7896e79e63c22adb094bfb28
RexJoush <jouah970918@sina.com>
2020-11-05 19:10:17 +0800

    2020.11.05

---
+++ src/November20/day29MinimumDepthOfBinaryTree/QueueNode.java
@@ -0,0 +1,11 @@
+package November20.day29MinimumDepthOfBinaryTree;
+
+public class QueueNode {
+    TreeNode node;
+    int depth;
+
+    public QueueNode(TreeNode node, int depth) {
+        this.node = node;
+        this.depth = depth;
+    }
+}
